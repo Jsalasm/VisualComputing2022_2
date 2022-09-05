44d05108e9ed64a33b835659a4b605c66a4aa068
Jsalasm <Jsalasm@users.noreply.github.com>
2022-09-05 08:35:19 +0000

    deploy: 8e4eb6be0efcfb0c19122b67a3a2fbf773d34c29

--- sketches/Taller1/illusion1-0.js
+++ sketches/Taller1/illusion1-0.js
@@ -4,23 +4,30 @@ const X_AXIS = 2;
 let b1, b2, c1, c2;
 let xMove =20;
 let moveRight =1;
-
+let checkbox;
+let isCheckedBox = false;
 function setup() {
   createCanvas(410, 400);
 
   // Definir colores
-  b1 = color(240);
+  b1 = color(230);
   b2 = color(140);
-  c12 = color(220);
+  c12 = color(180);
+
+  slider = createSlider(-10, 250, 140, 1);
+  slider.position(10, 10);
+
+  checkbox = createCheckbox('Animation', false);
+  checkbox.changed(myCheckedEvent);
 
 
 }
 
 function draw() {
   // Fondo
+  b2=color(slider.value())
   setGradient(0, 0, width, height, b1, b2, X_AXIS);
   setGradient(width, 0, width, height, b2, b1, X_AXIS);
-  // Frente
   
   
 }
@@ -35,14 +42,14 @@ function setGradient(x, y, w, h, c1, c2, axis) {
       let c = lerpColor(c1, c2, inter);
       stroke(c);
       line(i, y, i, y + h);
-      stroke(0);
+      stroke(5);
       
       fill(c12);
-      rect(20, 160, 120, 80);
-      rect(280, 160, 120, 80);
+      rect(20, 165, 80, 70);
+      rect(300, 165, 80, 70);
     }
    
-  if(xMove>280){
+  if(xMove>300){
     moveRight =0
   }
   if(xMove<20){
@@ -54,8 +61,11 @@ function setGradient(x, y, w, h, c1, c2, axis) {
   }else{
     xMove= xMove-1
   }
-  fill(c12);
-  rect(xMove, 250, 120, 80);
+  if(isCheckedBox){
+    fill(c12);
+    rect(xMove, 250, 80, 70);
+  }
+  
   
   }
 }
@@ -65,9 +75,11 @@ function drawRectangle(){
   stroke();
   fill(c1);
   rect(20, 160, 120, 80);
-  rect(280, 160, 120, 80);
+  rect(270, 160, 120, 80);
 }
 
-function drawRectangleMove(){
-  
+function myCheckedEvent() {
+
+    isCheckedBox=checkbox.checked()
+
 }
--- sketches/Taller1/illusion2-0.js
+++ sketches/Taller1/illusion2-0.js
@@ -4,38 +4,49 @@ var blueC = 238;
 var xC = [0,0,0,0,0,0];
 var yC =[0,0,0,0,0,0];
 
+let button;
+let valButton=0;
 function drawCircle(){
   for(var i =0; i<6;i++){
-    xC[i]=random(25,850)
-    yC[i] = random(25,850) 
+    xC[i]=random(50,550)
+    yC[i] = random(50,550) 
   }
 }
 
-function drawCircleInTime(){
-  for(var i = 0;i>3;i++){
-    circle(xC[i], yC[i],50) 
-    console.log(xC[i]);
+function changeBG() {
+  valButton += 1;
+  if(valButton>2){
+    valButton=0
   }
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
   setInterval(drawCircle,1000)
+
+  button = createButton('color');
+  button.position(0, 0);
+  button.mousePressed(changeBG);
 }
 
 function draw() {
   background(180);
-  fill(redC,greenC,blueC)
+  fill(100)
+  circle(280, 280,50)
+
   noStroke();
-  drawCircleInTime();
+  fill(redC,greenC,blueC)
+  if(valButton==2){
+    fill(255-redC,255-greenC,255-blueC)
+  }
   circle(xC[0], yC[0],50) 
   circle(xC[1], yC[1],50) 
   circle(xC[2], yC[2],50)
-  fill(2,255,27)
+  fill(255-redC,255-greenC,255-blueC)
+  if(valButton==1){
+    fill(redC,greenC,blueC)
+  }
   circle(xC[3], yC[3],50) 
   circle(xC[4], yC[4],50) 
   circle(xC[5], yC[5],50)
-  fill(140)
-  circle(400, 400,50)
-  
 }
