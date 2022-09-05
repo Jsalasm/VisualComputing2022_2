bf86704c6dc8f15867e50361d8e219907e94a92c
ccontrerasc <ccontrerasc@users.noreply.github.com>
2022-09-05 05:34:37 +0000

    deploy: c3e5bc29921587401be586b660fae9634ff38c90

---
+++ sketches/Taller1/tower.js
@@ -0,0 +1,90 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+    //first circle
+    for(var i=0, j=255; i<255, j>0; i++, j--) {
+        fill(i, j, i);
+    }
+    translate(width / 2, height / 2);
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('grey');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('white');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+  //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('grey');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('white');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+  //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('grey');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('white');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+  //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('grey');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('white');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+  //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('grey');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('white');
+    circle(10, 40, 100);
+    noStroke();
+    //
+    translate(p5.Vector.fromAngle(millis() / 800, 10));
+    fill('black');
+    circle(10, 40, 100);
+    noStroke();
+}
